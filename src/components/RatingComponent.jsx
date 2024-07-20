1462dd4aa69935d850af2704b5478a88e255d9db
Karan <email@karanshergill.com>
2024-07-20 17:47:48 +0530

    added custom colors and font

--- src/components/RatingComponent.jsx
+++ src/components/RatingComponent.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 export const RatingComponent = () => {
-  return (
-    <div>RatingComponent</div>
-  )
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <h1>How did we do?</h1>
+                        <p>Please let us know how we did with your support request. All feedback is appreciated
+                            to help us improve our offering!</p>
+                    </div>
+                    <div className='flex'>
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                        <div>4</div>
+                        <div>5</div>
+                    </div>
+                    <div>Submit</div>
+                </div>
+            </div>
+        </div>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Primary-Organge' : 'hsl(25, 97%, 53%)',
+        'Neutral-White' : 'hsl(0, 0%, 100%)',
+        'Neutral-Light-Grey' : 'hsl(217, 12%, 63%)',
+        'Neutral-Dark-Blue' : 'hsl(213, 19%, 18%)',
+        'Neutral-Very-Dark-Blue' :'hsl(216, 12%, 8%)'
+      },
+      fontFamily: {
+        'Overpass' : ['Overpass', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
-
